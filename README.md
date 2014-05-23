44eab59a4232c588bd3c811520cc8aa7255eb083
Damian Baćkowski <damianbackowski@gmail.com>
2014-05-23 16:55:23 +0200

    Fixed bugs, updated README.

--- README.md
+++ README.md
@@ -2,4 +2,30 @@
 
 Install this into your wordpress plugins/directory.
 
+## Configuration
+
+* URL - authentication url
+* Login param name - param name for login  
+* Password param name - param name for password
+
+**Example**
+
+URL: http://localhost
+Login param name: login
+Password param name: password
+
+This will make POST request when user log in to http://localhost with POST DATA: login = user provided login, password = user provided password
+
+## Authentication backend
+
+Authentication backend should return JSON response in format:
+
+**Authentication pass**
+
+{"status":1,"first_name":"John","last_name":"Doe","email":"john.doe@bar.com","login":"john_doe"}
+
+**Authentication fail** 
+
+{"status":0}
+
 Copyright (c) 2014 Damian Baćkowski, released under the MIT license
--- external_auth.php
+++ external_auth.php
@@ -26,19 +26,14 @@ function register_external_auth_settings() {
   register_setting( 'external-auth-settings', 'password_param_name' );
 }
 
-
 function external_auth($user, $username, $password) {
   if ($username == '' || $password == '') return;
 
-  $auth_url = sprintf("%s?%s=%s&%s=%s", get_option('authentication_url'), get_option('login_param_name'),
-                      $username, get_option('password_param_name'), $password);
-
-  $response = wp_remote_get($auth_url);
+  $data = array('body' => array(get_option('login_param_name') => $username, get_option('password_param_name') => $password));
+  $response = wp_remote_post(get_option('authentication_url'), $data);
   $auth_response = json_decode($response['body'], true);
 
-  if ($auth_response['status'] == 0) {
-    $user = new WP_Error('denied', __("Bad user/password"));
-  } else if($auth_response['result'] == 1) {
+  if($auth_response['status'] == 1) {
     $userobj = new WP_User();
     $user = $userobj->get_data_by('email', $auth_response['email']);
     $user = new WP_User($user->ID);
@@ -52,6 +47,8 @@ function external_auth($user, $username, $password) {
       $new_user_id = wp_insert_user($user_data);
       $user = new WP_User ($new_user_id);
     }
+  } else {
+    $user = new WP_Error('denied', __("Bad user/password"));
   }
 
   return $user;
